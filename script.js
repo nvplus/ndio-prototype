f89f35ca5225b8412626a1449d2fb4322775979d
root <root@DESKTOP-GJF3AN1.localdomain>
2020-06-13 20:30:05 -0400

    added page switching

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
+    $(".ndio-hf-button").click(function() {
+        $('.selected').removeClass('selected');
+        $(this).addClass("selected");
+    });
+
+    $('.ndio-home').click(function() {
+        $('.ndio-search-container').hide();
+        $('.ndio-home-container').show();
+        $('.ndio-profile-container').hide();
+    }); 
+
+    $('.ndio-search').click(function() {
+        $('.ndio-search-container').show();
+        $('.ndio-home-container').hide();
+        $('.ndio-profile-container').hide();
+    }); 
+
+    $('.ndio-profile').click(function() {
+        $('.ndio-search-container').hide();
+        $('.ndio-home-container').hide();
+        $('.ndio-profile-container').show();
+    }); 
     
     $(".poll-create").click(function() {
         alert("Poll create");
