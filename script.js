67736fe6cbf8818ca1133cb0e32111f97843fcf6
root <root@DESKTOP-GJF3AN1.localdomain>
2020-06-13 21:41:39 -0400

    added hardcoded comments

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
+function clearcomments() {
+    $('.ndio-comments').html("");
+}
 $(document).ready(function () {
+    /* POLLING */
+    $(".poll-create").click(function() {
+     
+    });
+
+    $(".poll-yes").click(function() {
+        
+    });
+    
+    $(".poll-no").click(function() {
+
+    });
+
+    $('.poll-open-comments').click(function() {
+        $('.ndio-home-container').hide();
+        clearcomments();
+
+       var num = Math.floor((Math.random() * 10) + 1);
+
+       for (var i = 0; i < num; i++) {
+            var n = i+1;
+            $('.ndio-comments').append("User " + n + ": Comment "+ n + "<br>");
+        }
+
+        $('.ndio-comments').show();
+    });
+
+    /* PAGE SWITCHING */
     $(".ndio-hf-button").click(function() {
         $('.selected').removeClass('selected');
         $(this).addClass("selected");
     });
 
     $('.ndio-home').click(function() {
+        clearcomments();
         $('.ndio-search-container').hide();
         $('.ndio-home-container').show();
         $('.ndio-profile-container').hide();
     }); 
 
     $('.ndio-search').click(function() {
+        clearcomments();
         $('.ndio-search-container').show();
         $('.ndio-home-container').hide();
         $('.ndio-profile-container').hide();
     }); 
 
     $('.ndio-profile').click(function() {
+        clearcomments();
         $('.ndio-search-container').hide();
         $('.ndio-home-container').hide();
         $('.ndio-profile-container').show();
     }); 
-    
-    $(".poll-create").click(function() {
-        alert("Poll create");
-    });
-
-    $(".poll-yes").click(function() {
-        alert("Poll no");
-    });
-    
-    $(".poll-no").click(function() {
-        alert("Poll no");
-    });
 });
